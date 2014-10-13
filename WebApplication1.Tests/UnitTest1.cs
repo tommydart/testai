db7041889406c190ac99c0bd5393cd23bc21f4af
Tomas Jankūnas <tommydart@gmail.com>
2014-10-13 20:08:45 +0300

    s

--- WebApplication1.Tests/UnitTest1.cs
+++ WebApplication1.Tests/UnitTest1.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 
 namespace WebApplication1.Tests
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
+
 }
