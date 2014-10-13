7e7f5949aaf5cb15846972df838784c98c0d2c11
Tomas Jankūnas <tommydart@gmail.com>
2014-10-13 18:37:39 +0300

    asdasd

--- ForFunT/Program.cs
+++ ForFunT/Program.cs
@@ -11,7 +11,7 @@ namespace ForFunT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world :)");
-            //kometas
+            //kometasa
         }
     }
 }
